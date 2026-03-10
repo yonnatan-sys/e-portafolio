095be00c65a4c95b3139cf8c72707e3e2af69c82
Erika Gomez <eygomez@unbosque.edu.co>
2026-03-09 22:05:22 -0500

    Fix rutas y foto de perfil para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://yonnatan-sys.github.io',
+	base: '/e-portafolio',
 	integrations: [mdx(), sitemap()],
 });
